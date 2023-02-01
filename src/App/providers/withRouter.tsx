390275527cb6d3fc2c4a5d08bc0aff20f2ce719b
OKhalanskyi <okhalanskyi@gmail.com>
2023-02-01 21:42:01 +0200

    feature : redux-toolkit , global styles , react-router added

---
+++ src/App/providers/withRouter.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+export const withRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <>hi</>,
+    },
+    {
+        path: '/1',
+        element: <>idi nah</>,
+    },
+])
